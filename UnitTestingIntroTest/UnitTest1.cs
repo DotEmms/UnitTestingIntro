273790ad38b4b029327058a8300a0f0256e8cd56
DotEmms <emmadanckaert@hotmail.com>
2021-01-22 14:59:47 +0100

    Add project files.

---
+++ UnitTestingIntroTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace UnitTestingIntroTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenTwoIntegers_WhenMultiplying_ThenResultEqualToMultiplication()
+        {
+            // ARRANGE - verzamel data
+            int numA = 5;
+            int numB = 7;
+
+            //ACT - doe iets
+            int result = numA * numB;
+
+            //ASSERT - vergelijk resultaat met verwacht resultaat
+            Assert.AreEqual(35, result);
+        }
+        [TestCase(5,7,12)]
+        [TestCase(10, 15, 25)]
+        [TestCase(2, 2, 4)]
+        public void GivenTwoIntegers_WhenAdding_ThenResultEqualToAddition(int numA, int numB, int expectedResult)
+        {
+            // ARRANGE - verzamel data
+            //See user input
+
+            //ACT - doe iets
+            int result = numA + numB;
+
+            //ASSERT - vergelijk resultaat met verwacht resultaat
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
